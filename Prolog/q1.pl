ab97fb787fd8263f2aea8330e0dd9388f21a75e5
Lucas de Castro <100042150+LuckZeera@users.noreply.github.com>
2023-06-27 17:09:18 -0300

    Update q1.pl

--- Prolog/q1.pl
+++ Prolog/q1.pl
@@ -1,4 +1,7 @@
  aluno_do_professor(X, Z) :-
     aluno(X, Y),
     professor(Z, Y).
-    
+
+pessoa_universidade(Pessoa, Universidade) :-
+    ((aluno(Pessoa, _) ; professor(Pessoa, _)), frequenta(Pessoa, Universidade)) ; 
+    ((funcionario(Pessoa, Universidade), professor(Pessoa, _))).
